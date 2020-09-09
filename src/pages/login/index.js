974bb79f84dc126142ae513dfed57de8ea74fd62
pray-lee <praycis@gmail.com>
2020-09-09 18:29:52 +0800

    update

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Form, FormGroup, FormControl, ControlLabel, Button, ButtonToolbar, Schema} from 'rsuite'
 import styled from './index.module.scss'
 
@@ -7,12 +7,26 @@ const {StringType} = Schema.Types
 const model = Schema.Model({
     name: StringType().isRequired('请填写用户名'),
     password: StringType().isRequired('请填写密码'),
-    imageCode: StringType().isRequired('请填写图片验证码')
+    // imageCode: StringType().isRequired('请填写图片验证码')
 })
 
 export default () => {
+    const [formValue, setFormValue] = useState({
+        name: '',
+        password: ''
+    })
+    // 提交
+    const handleLogin = () => {
+        console.log(formValue)
+    }
     return (
-        <Form fluid className={styled.container} model={model}>
+        <Form
+            fluid
+            className={styled.container}
+            model={model}
+            formValue={formValue}
+            onChange={formValue => setFormValue(formValue)}
+        >
             <FormGroup>
                 <ControlLabel>用户名</ControlLabel>
                 <FormControl name="name"/>
@@ -21,13 +35,13 @@ export default () => {
                 <ControlLabel>Password</ControlLabel>
                 <FormControl name="password" type="password"/>
             </FormGroup>
-            <FormGroup>
-                <ControlLabel>图片验证码</ControlLabel>
-                <FormControl name="imageCode"/>
-            </FormGroup>
+            {/*<FormGroup>*/}
+            {/*    <ControlLabel>图片验证码</ControlLabel>*/}
+            {/*    <FormControl name="imageCode"/>*/}
+            {/*</FormGroup>*/}
             <FormGroup>
                 <ButtonToolbar>
-                    <Button appearance="primary" block type="submit">
+                    <Button appearance="primary" block onClick={handleLogin}>
                         登录
                     </Button>
                 </ButtonToolbar>
